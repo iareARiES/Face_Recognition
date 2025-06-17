d17aad1b896aeb45450dd7fca41d8f5a3f8716be
Aditya <147093410+iareARiES@users.noreply.github.com>
2025-06-18 00:27:54 +0530

    Delete download_data.py

--- download_data.py
+++
@@ -1,7 +0,0 @@
-import kagglehub
-
-# Download latest version of the dataset
-path = kagglehub.dataset_download("lylmsc/wider-face-for-yolo-training")
-
-print("✅ Download complete!")
-print("📁 Dataset is available at:", path)
