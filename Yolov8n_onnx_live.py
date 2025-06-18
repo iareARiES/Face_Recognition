89da23fae60aed247aea39bc06e3efcd7ef7a642
Aditya <147093410+iareARiES@users.noreply.github.com>
2025-06-19 02:47:26 +0530

    Update Yolov8n_onnx_live.py

--- Yolov8n_onnx_live.py
+++ Yolov8n_onnx_live.py
@@ -7,17 +7,21 @@ onnx_model_path = "best.onnx"
 session = ort.InferenceSession(onnx_model_path)
 input_name = session.get_inputs()[0].name
 
+print("input_name:",input_name)
+
 # Preprocessing
 def preprocess(frame):
     img = cv2.resize(frame, (640, 640))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = img.astype(np.float32) / 255.0
-    img = np.transpose(img, (2, 0, 1))
-    img = np.expand_dims(img, axis=0).copy()
+    img = np.transpose(img, (2, 0, 1))  #Transposes shape from (H, W, C) → (C, H, W). as YOLOV8 expects in that way (C, H, W) → Channels x Height x Width (RGB, float32)
+    img = np.expand_dims(img, axis=0).copy()   # [Channels, Height, Width] ----> # [Batch, Channels, Height, Width]
     return img
 
+
 # Postprocessing for SINGLE class model (Face)
-def postprocess(outputs, orig_frame, conf_thres=0.3):
+# Purpose: Extracts boxes from model output and overlays them on the frame.
+def postprocess(outputs, orig_frame, conf_thres=0.2):
     predictions = outputs[0][0]  # (300, 6)
     orig_h, orig_w = orig_frame.shape[:2]
 
@@ -25,7 +29,7 @@ def postprocess(outputs, orig_frame, conf_thres=0.3):
     scale_y = orig_h / 640
 
     for pred in predictions:
-        obj_conf = pred[4]
+        obj_conf = pred[4]  #4 means 5th index thats for the prediction score
         if obj_conf < conf_thres:
             continue
 
@@ -34,14 +38,12 @@ def postprocess(outputs, orig_frame, conf_thres=0.3):
         x2 = int(pred[2] * scale_x)
         y2 = int(pred[3] * scale_y)
 
-        cv2.rectangle(orig_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.putText(orig_frame, f"Face", (x1, y1 - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+        score_text = f"Face: {obj_conf * 100:.1f}%" 
+        cv2.rectangle(orig_frame, (x1, y1), (x2, y2), (255, 0, 0), 4) 
+        cv2.putText(orig_frame, score_text, (x1, y1 - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)    #to display on the frame 
 
     return orig_frame
-
-
-
 
 # Start webcam
 cap = cv2.VideoCapture(0)
@@ -54,12 +56,25 @@ while True:
     if not ret:
         break
 
-    frame = cv2.flip(frame, 1)
-    input_tensor = preprocess(frame)
-    outputs = session.run(None, {input_name: input_tensor})
+    frame = cv2.flip(frame, 1)  #mirror flip
+    
+    input_tensor = preprocess(frame)  #does all the preprocessing 
+    '''Converts the image to:
+    640x640,RGB,Float32, normalized,Transposed to [1, 3, 640, 640]
+    This is the format your ONNX model expect'''
+    
+    outputs = session.run(None, {input_name: input_tensor})    #running the inferance , Feeds the preprocessed tensor into the model.
+    
     print(f"ONNX Output shape: {outputs[0].shape}")
+    print(outputs)
 
     annotated = postprocess(outputs, frame)
+    '''This line takes the raw model outputs (outputs) and the original image/frame (frame), and processes them to:
+    - Filter detections (e.g., confidence > 0.2)
+    - Apply Non-Maximum Suppression (NMS) to remove overlapping boxes
+    - Draw bounding boxes and labels on the frame (usually with cv2.rectangle() and cv2.putText())'''
+    
+    #annotated is the same image frame, but with boxes and labels drawn on detected objects — ready for display.
 
     cv2.imshow("YOLOv8 ONNX Face Detection", annotated)
     if cv2.waitKey(1) & 0xFF == ord("q"):
